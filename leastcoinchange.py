d11da4c3232c71b4a7a5d83abbbda6ddb2a75613
ijoosong <ijoosong@gmail.com>
2016-09-06 16:05:04 -0400

    added my solution

--- leastcoinchange.py
+++ leastcoinchange.py
@@ -19,3 +19,19 @@ def coin_change(cash):
 			final_count[0] += 1
 			amt_left -= 1
 	return final_count, sum(final_count)
+	
+def joes_solution(coins, value):
+    table = [None for x in range(value + 1)]
+    table[0] = []
+    for i in range(1, value + 1):
+        for coin in coins:
+            if coin > i: continue
+            elif not table[i] or len(table[i - coin]) + 1 < len(table[i]):
+                if table[i - coin] != None:
+                    table[i] = table[i - coin][:]
+                    table[i].append(coin)
+
+    if table[-1] != None:
+        print '%d coins: %s' % (len(table[-1]), table[-1])
+    else:
+        print 'No solution possible'
